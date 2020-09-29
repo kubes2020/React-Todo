adba8cb9c0207c75fe8506e6bf2d29e0d774b152
Brian Kubes <briankubes@gmail.com>
2020-09-28 19:51:55 -0700

    line-through text is working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./Todo.css";
 
 const Todo = (props) => {
   return (
     <>
-      <div onClick={() => props.toggleItem(props.tod.id)}>
+      <div
+        onClick={() => props.toggleItem(props.tod.id)}
+        className={`item${props.tod.completed ? ` completed` : ""}`}
+      >
         <p>{props.tod.title}</p>
       </div>
     </>
